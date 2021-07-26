83ec4f5d0d3277d166c2db1bf94a5c59bbd9e227
Avinash Maskey <avinash.maskey11@gmail.com>
2021-07-26 12:32:56 +0545

    dd

--- chapter-seven/CRUD-ALL/insert.php
+++ chapter-seven/CRUD-ALL/insert.php
@@ -10,26 +10,25 @@
 //databse connection
 require_once 'connect.php';
 
-if(isset($_POST) && !empty($_POST)){
+if (isset($_POST) && !empty($_POST)) {
     $sql = "INSERT INTO students
     SET
-    name = '".$_POST['uname']."', 
-    email = '".$_POST['uemail']."', 
-    address = '".$_POST['uaddress']."', 
-    number = '".$_POST['unum']."' 
+    name = '" . $_POST['uname'] . "', 
+    email = '" . $_POST['uemail'] . "', 
+    address = '" . $_POST['uaddress'] . "', 
+    number = '" . $_POST['unum'] . "' 
     ";
 
-//executing a query in database
-$query = mysqli_query($conn, $sql); 
+    //executing a query in database
+    $query = mysqli_query($conn, $sql);
 
-if($query){
-//success
-echo "Data Inserted Successfully.";
-}else{
-echo mysqli_error($conn);
-}
-
-}else{
+    if ($query) {
+        //success
+        echo "Data Inserted Successfully.";
+    } else {
+        echo mysqli_error($conn);
+    }
+} else {
     header('location: form.php');
     exit;
 }
